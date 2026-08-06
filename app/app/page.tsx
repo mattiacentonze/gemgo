7b56b2bb0f5a25833230078070faabe5c8e73daa
Mattia Centonze <93384261+mattiacentonze@users.noreply.github.com>
2026-08-07 01:15:37 +0200

    Mount post-visit feedback

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,6 +3,7 @@ import IntegratedAppShell from "../components/IntegratedAppShell";
 import MobileResultsMode from "../components/MobileResultsMode";
 import NotificationCenter from "../components/NotificationCenter";
 import PrivacyControls from "../components/PrivacyControls";
+import VisitFeedback from "../components/VisitFeedback";
 
 export default function GemGoApplicationPage() {
   return (
@@ -12,6 +13,7 @@ export default function GemGoApplicationPage() {
       <NotificationCenter />
       <MobileResultsMode />
       <PrivacyControls />
+      <VisitFeedback />
     </>
   );
 }
