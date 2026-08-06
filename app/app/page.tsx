6201c4edeff973f94d7a770b98d19f55284c42d7
Mattia Centonze <93384261+mattiacentonze@users.noreply.github.com>
2026-08-07 01:21:42 +0200

    Mount optional interface sound controller

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,6 +3,7 @@ import IntegratedAppShell from "../components/IntegratedAppShell";
 import MobileResultsMode from "../components/MobileResultsMode";
 import NotificationCenter from "../components/NotificationCenter";
 import PrivacyControls from "../components/PrivacyControls";
+import UiSoundController from "../components/UiSoundController";
 import VisitFeedback from "../components/VisitFeedback";
 
 export default function GemGoApplicationPage() {
@@ -13,6 +14,7 @@ export default function GemGoApplicationPage() {
       <NotificationCenter />
       <MobileResultsMode />
       <PrivacyControls />
+      <UiSoundController />
       <VisitFeedback />
     </>
   );
