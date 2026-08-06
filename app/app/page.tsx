712ac583cba38afab1edee821e35ab1d2d916379
Mattia Centonze <93384261+mattiacentonze@users.noreply.github.com>
2026-08-07 01:06:17 +0200

    Mount notification center in the application

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,11 @@
 import IntegratedAppShell from "../components/IntegratedAppShell";
+import NotificationCenter from "../components/NotificationCenter";
 
 export default function GemGoApplicationPage() {
-  return <IntegratedAppShell />;
+  return (
+    <>
+      <IntegratedAppShell />
+      <NotificationCenter />
+    </>
+  );
 }
